52d5990488d3abece95bda0015945853cf89f516
Cris R <cristofer@gmail.com>
2019-05-31 11:50:44 +0200

    Adding first routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,22 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  namespace :api do
+    namespace :v1 do
+      namespace :game do
+        post 'create', to: 'game#create'
+        get ':id/status', to: 'game#status'
+
+        post ':game_id/score/create', to: 'score#create'
+        get ':game_id/score/show', to: 'score#show'
+      end
+    end
+  end
+  
+  get '*page', to: 'welcome#index', constraints: -> (req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root 'welcome#index'
 end
