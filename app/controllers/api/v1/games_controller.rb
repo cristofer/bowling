bdb6b3502acdbf5edd7d00376473016c5a3be259
Cris R <cristofer@gmail.com>
2019-05-31 14:30:18 +0200

    Adding validations for Name in Game. Adding tests for the Controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -3,9 +3,12 @@ module Api
     class GamesController < Base
       def create
         render json: serializer.new(create_game), status: :created
+      rescue ActiveRecord::RecordInvalid => e
+        logger.fatal "ERROR: Games#create RecordInvalid: #{e}"
+        render json: json_record_invalid_error, status: :bad_request
       rescue StandardError => e
-        logger.fatal "ERROR: Games#create: #{e}"
-        render json: json_error, status: 500 # conflict
+        logger.fatal "ERROR: Games#create: StandardError: #{e}"
+        render json: json_error, status: :internal_server_error
       end
 
       private
@@ -18,7 +21,11 @@ module Api
         params[:name]
       end
 
-      def json_error
+      def json_record_invalid_error
+        { data: { error: 'Name can not be empty' } }
+      end
+
+      def json_standard_error
         { data: { error: 'There was an internal error, we could not create the Game' } }
       end
 
--- test/controllers/api/v1/games_controller_test.rb
+++ test/controllers/api/v1/games_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class Api::V1::GamesControllerTest < ActionDispatch::IntegrationTest
-  test 'it creates a new Game' do
+  test 'it creates a new Game with a valid name' do
     name = 'New Name'
 
     post api_v1_games_create_path, params: { name: name }
@@ -11,5 +11,15 @@ class Api::V1::GamesControllerTest < ActionDispatch::IntegrationTest
     body = JSON.parse(response.body)
 
     assert_equal body['data']['attributes']['name'], name
+  end
+
+  test 'it does not create a new Game when name is empty' do
+    post api_v1_games_create_path, params: { name: '' }
+
+    assert_response :bad_request
+
+    body = JSON.parse(response.body)
+
+    assert_equal body['data']['error'], 'Name can not be empty'
   end
 end
