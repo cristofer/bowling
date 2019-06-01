5e498a2ea25dafbf2de7da4b584f949d880ce8aa
Cris R <cristofer@gmail.com>
2019-06-01 12:51:48 +0200

    Adding the game logic for counting the points of the Frame

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -18,13 +18,18 @@ class Frame < ApplicationRecord
   belongs_to :game
 
   after_update :check_strike_or_spare
+  after_update :calculate_total
+  after_update :calculate_previous_total, if: :frame_has_finished?
+  after_update :update_current_frame_for_game, if: :frame_has_finished?
 
+  # @return Frame: the previous frame if the current_one is not the first one
   def previous_frame
     return nil if self.number == 1
 
     game.frames.find_by(number: self.number - 1)
   end
 
+  # @return Frame: the next frame if the current_one is not the last one
   def next_frame
     return nil if self.number == 10
 
@@ -33,8 +38,59 @@ class Frame < ApplicationRecord
 
   private
 
+  # It checks if the current_frame should be marked either as strike or spare
   def check_strike_or_spare
-    self.strike = true and return if first_roll == 10
-    self.spare = true if first_roll + second_roll == 10
+    self.update_columns(strike: true, total: 10, second_roll: 0) and return if first_roll == 10
+    self.update_columns(spare: true, total: 10) if first_roll + second_roll == 10
+  end
+
+  # It updates the total once both rolls have been played
+  def calculate_total
+    return if strike_or_spare?
+
+    return unless both_rolls_played?
+
+    self.update_columns(total: first_roll + second_roll)
+  end
+
+  # It sets the previous total when corresponding (prev frame strike or sspare)
+  def calculate_previous_total
+    return if number == 1 # nothing to calculate if it is the first frame
+
+    return unless previous_frame.strike || previous_frame.spare
+
+    if previous_frame.strike
+      new_total = first_roll + second_roll + previous_frame.total
+      previous_frame.update_columns(total: new_total)
+    end
+
+    if previous_frame.spare
+      new_total = first_roll + previous_frame.total
+      previous_frame.update_columns(total: new_total)
+    end
+  end
+
+  # @return Boolean: either if the current frame is spare or strike
+  def strike_or_spare?
+    strike || spare
+  end
+
+  # @return Boolean: true when boths rolls has valid points (positive)
+  def both_rolls_played?
+    first_roll.positive? && second_roll.positive?
+  end
+
+  # @return Boolean: when one of the rolls is still negative (not played)
+  def still_rolls_unplayed?
+    !both_rolls_played?
+  end
+
+  def frame_has_finished?
+    strike_or_spare? || both_rolls_played?
+  end
+
+  # It updates the current_frame for the game
+  def update_current_frame_for_game
+    game.update_columns(current_frame_id: next_frame.id) if next_frame.present?
   end
 end
--- test/models/frame_test.rb
+++ test/models/frame_test.rb
@@ -56,4 +56,134 @@ class FrameTest < ActiveSupport::TestCase
     assert_nil frame.number
     assert_equal frame.game, games(:one)
   end
+
+  test 'it sets the strike to true when first_roll is 10 (spare should be false)' do
+    frame_1 = games(:one).frames.first
+
+    frame_1.first_roll = 10
+    frame_1.save!
+
+    assert_equal frame_1.strike, true
+    assert_equal frame_1.spare, false
+    assert_equal frame_1.total, 10
+  end
+
+  test 'it sets the spare to true when total is 10 (strike should be false)' do
+    frame_1 = games(:one).frames.first
+
+    frame_1.first_roll = 3
+    frame_1.second_roll = 7
+    frame_1.save!
+
+    assert_equal frame_1.spare, true
+    assert_equal frame_1.strike, false
+    assert_equal frame_1.total, 10
+  end
+
+  test 'it updates total of a striked frame' do
+    new_game = Game.create(name: 'New')
+
+    frame_1 = new_game.frames.first
+    frame_2 = new_game.frames.second
+
+    frame_1.first_roll = 10
+    frame_1.save!
+
+    frame_2.first_roll = 2
+    frame_2.second_roll = 3
+    frame_2.save!
+
+    assert_equal frame_1.reload.total, 15
+  end
+
+  test 'it updates total of a spared frame' do
+    new_game = Game.create(name: 'New')
+
+    frame_1 = new_game.frames.first
+    frame_2 = new_game.frames.second
+
+    frame_1.first_roll = 5
+    frame_1.second_roll = 5
+    frame_1.save!
+
+    frame_2.first_roll = 2
+    frame_2.second_roll = 3
+    frame_2.save!
+
+    assert_equal frame_1.reload.total, 12
+  end
+
+  test 'it doesnt update the total of prev frame when no striked or spared' do
+    new_game = Game.create(name: 'New')
+
+    frame_1 = new_game.frames.first
+    frame_2 = new_game.frames.second
+
+    frame_1.first_roll = 1
+    frame_1.second_roll = 2
+    frame_1.save!
+
+    frame_2.first_roll = 2
+    frame_2.second_roll = 3
+    frame_2.save!
+
+    assert_equal frame_1.reload.total, 3
+  end
+
+  test 'it sets the total correctly' do
+    frame = games(:one).frames.first
+
+    frame.first_roll = 2
+    frame.second_roll = 5
+    frame.save!
+
+    assert_equal frame.total, 7
+  end
+
+  test 'it retries the correct previous_frame' do
+    new_game = Game.create(name: 'New')
+    frames = new_game.frames
+
+    assert_nil frames.first.previous_frame
+    assert_equal frames.second.previous_frame, frames.first
+  end
+
+  test 'it retries the correct next_frame' do
+    new_game = Game.create(name: 'New')
+    frames = new_game.frames
+
+    assert_nil frames.last.next_frame
+    assert_equal frames.first.next_frame, frames.second
+  end
+
+  # TODO: update the current_frame_id for the game
+
+  test 'it has the correct current_frame_id when game is created' do
+    new_game = Game.create(name: 'New')
+
+    assert_equal new_game.current_frame_id, new_game.frames.first.id
+  end
+
+  test 'it has the correct current_frame_id when the current_frame is ended' do
+    new_game = Game.create(name: 'New')
+    first_frame = new_game.frames.first
+
+    first_frame.first_roll = 10
+    first_frame.save!
+
+    assert_equal new_game.current_frame_id, new_game.frames.second.id
+  end
+
+  test 'it keeps the current_frame_id of the last fram when game is in the last frame' do
+    new_game = Game.create(name: 'New')
+
+    last_frame = new_game.frames.last
+
+    new_game.current_frame_id = last_frame.id
+
+    last_frame.first_roll = 10
+    last_frame.save!
+
+    assert_equal new_game.current_frame_id, last_frame.id
+  end
 end
